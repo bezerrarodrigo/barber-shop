e778954eecc6c209e1a59468dbc0f50ae77edfca
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-05-16 11:25:32 -0300

    feat: add image buttons for hair services and enhance home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from '@/components/Header/header';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SearchIcon } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -10,12 +11,41 @@ export default function Home() {
       <div className="p-5">
         <h2 className="text-2xl font-bold">Olá, Bezerra!</h2>
         <p className="text-sm">Sexta-feira, 15 de maio.</p>
-      </div>
-      <div className="flex gap-2 px-5">
-        <Input placeholder="Faça a sua busca..." />
-        <Button size="icon">
-          <SearchIcon />
-        </Button>
+        <div className="flex gap-2 mt-6">
+          <Input placeholder="Faça a sua busca..." />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="mt-6 flex justify-between gap-3">
+          <Button variant="outline">
+            <Image
+              src="/scissors.png"
+              alt="Scissors icon"
+              width={14}
+              height={14}
+            />
+            Cabelo
+          </Button>
+          <Button variant="outline">
+            <Image
+              src="/mustache.png"
+              alt="Scissors icon"
+              width={14}
+              height={14}
+            />
+            Barba
+          </Button>
+          <Button variant="outline">
+            <Image
+              src="/razor.png"
+              alt="Scissors icon"
+              width={14}
+              height={14}
+            />
+            Acabamento
+          </Button>
+        </div>
       </div>
     </div>
   );
