15a4e83b418c584dfa3659b30a8023459d047cb3
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-05-16 15:08:30 -0300

    feat: add Avatar and Badge components for enhanced user interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { SearchIcon } from 'lucide-react';
 import Image from 'next/image';
@@ -52,6 +55,29 @@ export default function Home() {
             alt="Agende com os melhores profissionais"
           />
         </div>
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex justify-center items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png"
+                    alt="@vintagebarber"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm font-light">9h45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
