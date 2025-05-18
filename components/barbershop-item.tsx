22d0f634d1d89ffbf9045f8c7b36738815d9afbc
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-05-18 11:26:03 -0300

    feat: add badge component with rating to barber shop item display

--- components/barbershop-item.tsx
+++ components/barbershop-item.tsx
@@ -2,6 +2,8 @@ import { BarberShop } from '@/lib/generated/prisma';
 import Image from 'next/image';
 import { Card, CardContent } from './ui/card';
 import { Button } from './ui/button';
+import { Badge } from './ui/badge';
+import { StarIcon } from 'lucide-react';
 
 interface BarberShopItemProps {
   barber: BarberShop;
@@ -11,13 +13,17 @@ export function BarberShopItem({ barber }: BarberShopItemProps) {
   return (
     <Card className="min-w-[159px] py-2">
       <CardContent className="px-2">
-        <div className="relative h-[100px] w-full">
+        <div className="relative h-[159px] w-full">
           <Image
             src={barber.imageUrl}
             alt={barber.name}
             fill
             className="object-cover rounded-lg"
           />
+          <Badge variant="secondary" className="absolute top-2 left-2">
+            <StarIcon className="text-primary fill-primary" />
+            <span className="text-xs">4.5</span>
+          </Badge>
         </div>
         <div className="my-2">
           <h3 className="font-semibold truncate">{barber.name}</h3>
